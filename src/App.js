392118af6597a8c722a2ca9d32e25b8ba9a25c48
Victor <career.victorobije@gmail.com>
2022-11-29 12:36:36 +0100

    todo-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+/** Question: TodoList also accepts an onItemClick function prop, which should be called when a user 
+ * clicks an item in the list, if the item is not marked as done. Otherwise, the onItemClick 
+ * should not be called and the click event itself should not be propagated further. 
+ * The function should be called with the item object from the items list as the first parameter and the event as the second parameter. */
+
+
+
+const TodoItem = (props) => <li onClick={props.onClick}>{props.item.text}</li>
+
+const TodoList = ({ items, onListClick, onItemClick }) => {
+
+
+    const   handleItemClick = (item, event) => {
+      // Write your code here
+      if (item.done){
+        event.stopPropagation()
+        return
+      }
+      onItemClick(item, event)
+      
+    }
+    return (<ul onClick={onListClick}>
+      {items.map((item, index) => 
+        <TodoItem item={item} key={index} onClick={(event) => handleItemClick(item, event)}/>)}
+    </ul>);
+
 }
+
+
+const items = [ { text: 'Buy grocery', done: true },
+  { text: 'Play guitar', done: false },
+  { text: 'Romantic dinner', done: false }
+];
+
+const App = (props) => <TodoList
+  items={items}
+  onListClick={(event) => console.log("List clicked!")}
+  onItemClick={(item, event) => { console.log(item, event) }}
+/>;
 
 export default App;
