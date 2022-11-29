b3c19e1f9253f56aa52d0e0af9e085bfb1142d9f
Victor <career.victorobije@gmail.com>
2022-11-29 12:09:47 +0100

    image-gallery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+
+
+/** QUESTION: Implement the ImageGallery component that accepts a links prop and renders the gallery 
+ * described above so that the first item in the links prop is the src attribute of the first image in the gallery. 
+ * It should also implement the following logic: When the button is clicked, the image 
+ * that is in the same div as the button should be removed from the gallery. */
+
+
+
+
+const ImageGallery = ({ links, handleButtonClick }) => {
+  return links.map((link) => (
+    <div key={link}>
+      <div className="image">
+        <img alt="link" src={link} />
+        <button onClick={() => handleButtonClick(link)} class="remove">
+          X
+        </button>
+      </div>
+    </div>
+  ));
+};
 
 function App() {
+  let links = ["https://bit.ly/3lmYVna", "https://bit.ly/3flyaMj"];
+  const [linksUrl, setlinksUrl] = useState(links);
+
+  const removeLink = (link) => {
+    let updatedList = linksUrl.filter((image) => link !== image);
+    setlinksUrl(updatedList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageGallery links={linksUrl} handleButtonClick={removeLink} />
     </div>
   );
 }
